013b9033cc16c918d7db6fca37c87c24600f3c75
Leonan Luppi <leonan.luppi@gmail.com>
2018-08-30 11:25:19 -0300

    init

---
+++ mean-express/src/api/posts/post-show-get.js
@@ -0,0 +1,15 @@
+const Post = require('./../../models/post');
+
+module.exports = async (req, res) => {
+    const { slug } = req.params;
+    if (!slug) {
+        return res.status(404).end();
+    }
+
+    try {
+        const post = await Post.findOne({ slug });
+        return res.status(200).json({ post: post || {} });
+    } catch (err) {
+        return res.status(500).json({ err });
+    }
+}
